c785eced9dd160a8007ab7700b3b0578e8600bc8
meew0 <blactbt@live.de>
2016-09-03 00:37:10 +0200

    Use the manual initializer for WebSocket
    Otherwise the query string would get omitted and we would land on gateway v3.
    (Apparently that still works...)

--- src/discordcr/client.cr
+++ src/discordcr/client.cr
@@ -10,9 +10,14 @@ module Discordcr
     end
 
     def run
-      url = gateway.url
-      url += "?v=6&encoding=json"
-      @websocket = websocket = HTTP::WebSocket.new(URI.parse(url))
+      url = URI.parse(gateway.url)
+      @websocket = websocket = HTTP::WebSocket.new(
+        host: url.host.not_nil!,
+        path: "#{url.path}/?encoding=json&v=6",
+        port: 443,
+        tls: true
+      )
+
       websocket.on_message(&->on_message(String))
       websocket.on_close(&->on_close(String))
       websocket.run
@@ -24,6 +29,7 @@ module Discordcr
     end
 
     private def on_message(message : String)
+      puts message
     end
   end
 end
