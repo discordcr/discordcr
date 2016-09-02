44a328f1d2c355dc0ce0f774879de338c8dcdda3
meew0 <blactbt@live.de>
2016-09-02 22:36:51 +0200

    Add an Authorization header to the request method

--- src/discordcr/rest.cr
+++ src/discordcr/rest.cr
@@ -3,6 +3,7 @@ require "http/client"
 module Discordcr
   module REST
     def request(endpoint_key : Symbol, method : String, url : String | URI, headers : HTTP::Headers | Nil, body : String | Nil)
+      headers["Authorization"] = @token
       HTTP::Client.exec(method: method, url: url, headers: headers, body: body, tls: true)
     end
   end
