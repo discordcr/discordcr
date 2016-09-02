685532903fb659b7294aa552fa295fac39fdf8b7
meew0 <blactbt@live.de>
2016-09-02 23:40:03 +0200

    Run the websocket after registering the message handler

--- src/discordcr/client.cr
+++ src/discordcr/client.cr
@@ -14,6 +14,7 @@ module Discordcr
       url += "?v=6&encoding=json"
       @websocket = HTTP::WebSocket.new(URI.parse(url))
       @websocket.not_nil!.on_message(&->on_message(String)) # TODO: better error handling
+      @websocket.not_nil!.run
     end
 
     private def on_message(message : String)
