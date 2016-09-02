979cbfaba0e3e9673fd6bac0dcc8d83d258f43dd
meew0 <blactbt@live.de>
2016-09-02 23:14:43 +0200

    Use a proper SSL context instead of `true`

--- src/discordcr/rest.cr
+++ src/discordcr/rest.cr
@@ -1,12 +1,14 @@
 require "http/client"
+require "openssl/ssl/context"
 
 require "./mappings"
 
 module Discordcr
   module REST
+    SSL_CONTEXT = OpenSSL::SSL::Context::Client.new
+
     def request(endpoint_key : Symbol, method : String, url : String | URI, headers : HTTP::Headers | Nil, body : String | Nil)
-      headers["Authorization"] = @token
-      HTTP::Client.exec(method: method, url: url, headers: headers, body: body, tls: true)
+      HTTP::Client.exec(method: method, url: url, headers: headers, body: body, tls: SSL_CONTEXT)
     end
 
     API_BASE = "https://discordapp.com/api/v6"
