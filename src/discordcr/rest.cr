8a61c3c8d812cf1ab88296a80130de8bfb8ac222
meew0 <blactbt@live.de>
2016-09-02 22:34:07 +0200

    Implement a very basic request method

--- src/discordcr/rest.cr
+++ src/discordcr/rest.cr
@@ -1,4 +1,9 @@
+require "http/client"
+
 module Discordcr
   module REST
+    def request(endpoint_key : Symbol, method : String, url: String | URI, headers : HTTP::Headers | Nil, body : String | Nil)
+      HTTP::Client.exec(method: method, url: url, headers: headers, body: body, tls: true)
+    end
   end
 end
