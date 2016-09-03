5bc400ee2ef3aa01bd20354a88e387b71072b620
meew0 <blactbt@live.de>
2016-09-03 15:05:28 +0200

    Write a test for SnowflakeConverter

--- spec/discordcr_spec.cr
+++ spec/discordcr_spec.cr
@@ -1,4 +1,18 @@
 require "./spec_helper"
 
+struct StructWithSnowflake
+  JSON.mapping(
+    data: {type: UInt64, converter: Discord::SnowflakeConverter}
+  )
+end
+
 describe Discord do
+  describe Discord::SnowflakeConverter do
+    it "converts a string to u64" do
+      json = %({"data":"10000000000"})
+
+      obj = StructWithSnowflake.from_json(json)
+      obj.data.should eq 10000000000
+    end
+  end
 end
