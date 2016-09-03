c0ebf0ac9034574c785eaf33f7e03540611022c2
meew0 <blactbt@live.de>
2016-09-03 15:22:12 +0200

    Add a mapping for voice states

--- src/discordcr/mappings.cr
+++ src/discordcr/mappings.cr
@@ -119,6 +119,20 @@ module Discord
     )
   end
 
+  struct VoiceState
+    JSON.mapping(
+      guild_id: {type: UInt64 | Nil, converter: MaybeSnowflakeConverter},
+      channel_id: {type: UInt64, converter: SnowflakeConverter},
+      user_id: {type: UInt64, converter: SnowflakeConverter},
+      session_id: String,
+      deaf: Bool,
+      mute: Bool,
+      self_deaf: Bool,
+      self_mute: Bool,
+      suppress: Bool
+    )
+  end
+
   struct UnavailableGuild
     JSON.mapping(
       id: {type: UInt64, converter: SnowflakeConverter},
