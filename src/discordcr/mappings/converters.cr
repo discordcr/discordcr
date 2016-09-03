96197cf703d049c949c28e74fe6dbe11872359cf
meew0 <blactbt@live.de>
2016-09-03 16:27:48 +0200

    Remove an unnecessary not_nil! from SnowflakeConverter

--- src/discordcr/mappings/converters.cr
+++ src/discordcr/mappings/converters.cr
@@ -4,8 +4,7 @@ require "time/format"
 module Discord
   module SnowflakeConverter
     def self.from_json(parser : JSON::PullParser) : UInt64
-      str = parser.read_string_or_null
-      str.not_nil!.to_u64
+      parser.read_string.to_u64
     end
 
     def self.to_json(value : UInt64, io : IO)
