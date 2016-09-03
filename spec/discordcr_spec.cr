a7d2ad0de9b4d0894dd33804255c869b19b8b535
meew0 <blactbt@live.de>
2016-09-03 15:06:53 +0200

    Write a test for MaybeSnowflakeConverter

--- spec/discordcr_spec.cr
+++ spec/discordcr_spec.cr
@@ -6,6 +6,12 @@ struct StructWithSnowflake
   )
 end
 
+struct StructWithMaybeSnowflake
+  JSON.mapping(
+    data: {type: UInt64 | Nil, converter: Discord::MaybeSnowflakeConverter}
+  )
+end
+
 describe Discord do
   describe Discord::SnowflakeConverter do
     it "converts a string to u64" do
@@ -13,6 +19,22 @@ describe Discord do
 
       obj = StructWithSnowflake.from_json(json)
       obj.data.should eq 10000000000
+    end
+  end
+
+  describe Discord::MaybeSnowflakeConverter do
+    it "converts a string to u64" do
+      json = %({"data":"10000000000"})
+
+      obj = StructWithMaybeSnowflake.from_json(json)
+      obj.data.should eq 10000000000
+    end
+
+    it "converts null to nil" do
+      json = %({"data":null})
+
+      obj = StructWithMaybeSnowflake.from_json(json)
+      obj.data.should eq nil
     end
   end
 end
