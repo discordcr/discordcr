d43653cc29f37dd21016439a31b8b04d49b11dc6
meew0 <blactbt@live.de>
2016-09-07 18:33:17 +0200

    Add an event for the GUILD_ROLE_UPDATE dispatch

--- src/discordcr/client.cr
+++ src/discordcr/client.cr
@@ -170,6 +170,9 @@ module Discord
       when "GUILD_ROLE_CREATE"
         payload = Gateway::GuildRolePayload.from_json(data)
         call_event guild_role_create, payload
+      when "GUILD_ROLE_UPDATE"
+        payload = Gateway::GuildRolePayload.from_json(data)
+        call_event guild_role_update, payload
       when "MESSAGE_CREATE"
         payload = Message.from_json(data)
         puts "Received message with content #{payload.content}"
@@ -207,6 +210,7 @@ module Discord
     event guild_members_chunk, Gateway::GuildMembersChunkPayload
 
     event guild_role_create, Gateway::GuildRolePayload
+    event guild_role_update, Gateway::GuildRolePayload
 
     event message, Message
   end
