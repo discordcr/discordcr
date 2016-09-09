ebcd734580a14dd801147bc036a9849c6e1f7f0f
meew0 <blactbt@live.de>
2016-09-09 23:49:16 +0200

    Add support for channel caching

--- src/discordcr/cache.cr
+++ src/discordcr/cache.cr
@@ -4,14 +4,23 @@ module Discord
   class Cache
     def initialize(@client : Client)
       @users = Hash(UInt64, User).new
+      @channels = Hash(UInt64, Channel).new
     end
 
     def resolve_user(id : UInt64) : User
       @users.fetch(id) { @users[id] = @client.get_user(id) }
     end
 
+    def resolve_channel(id : UInt64) : Channel
+      @channels.fetch(id) { @channels[id] = @client.get_channel(id) }
+    end
+
     def cache(user : User)
       @users[user.id] = user
+    end
+
+    def cache(channel : Channel)
+      @channels[channel.id] = channel
     end
   end
 end
