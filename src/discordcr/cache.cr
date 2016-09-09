752d0bd8758aecdaa86960a2c7cafab94c669609
meew0 <blactbt@live.de>
2016-09-09 22:36:26 +0200

    Implement user resolution

--- src/discordcr/cache.cr
+++ src/discordcr/cache.cr
@@ -1,6 +1,18 @@
+require "./mappings/*"
+
 module Discord
   class Cache
     def initialize(@client : Client)
+      @users = Hash(UInt64, User).new
+    end
+
+    def resolve_user(id : UInt64) : User
+      user = @users[id]?
+      return user if user
+
+      user = @client.get_user(id)
+      @users[id] = user
+      user
     end
   end
 end
