75a46918c7f8218bb6f907c4330d3abeb7092d80
meew0 <blactbt@live.de>
2016-09-09 23:50:09 +0200

    Add support for guild caching

--- src/discordcr/cache.cr
+++ src/discordcr/cache.cr
@@ -5,6 +5,7 @@ module Discord
     def initialize(@client : Client)
       @users = Hash(UInt64, User).new
       @channels = Hash(UInt64, Channel).new
+      @guilds = Hash(UInt64, Guild).new
     end
 
     def resolve_user(id : UInt64) : User
@@ -15,12 +16,20 @@ module Discord
       @channels.fetch(id) { @channels[id] = @client.get_channel(id) }
     end
 
+    def resolve_guild(id : UInt64) : Guild
+      @guilds.fetch(id) { @guilds[id] = @client.get_guild(id) }
+    end
+
     def cache(user : User)
       @users[user.id] = user
     end
 
     def cache(channel : Channel)
       @channels[channel.id] = channel
+    end
+
+    def cache(guild : Guild)
+      @guilds[guild.id] = guild
     end
   end
 end
