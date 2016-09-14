8f3db20373ad830059a1c51a763ee1ba27a767f1
meew0 <blactbt@live.de>
2016-09-14 18:55:39 +0200

    Add all the opcodes as constants

--- src/discordcr/client.cr
+++ src/discordcr/client.cr
@@ -32,8 +32,18 @@ module Discord
       puts "Closed with: " + message
     end
 
-    OP_DISPATCH =  0
-    OP_HELLO    = 10
+    OP_DISPATCH              =  0
+    OP_HEARTBEAT             =  1
+    OP_IDENTIFY              =  2
+    OP_STATUS_UPDATE         =  3
+    OP_VOICE_STATE_UPDATE    =  4
+    OP_VOICE_SERVER_PING     =  5
+    OP_RESUME                =  6
+    OP_RECONNECT             =  7
+    OP_REQUEST_GUILD_MEMBERS =  8
+    OP_INVALID_SESSION       =  9
+    OP_HELLO                 = 10
+    OP_HEARTBEAT_ACK         = 11
 
     private def on_message(message : String)
       spawn do
