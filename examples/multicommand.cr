afa856283067e0c0563676c5c3297ae5ef4ee61b
Colin Rioux <colin350@gmail.com>
2016-10-12 14:30:21 -0400

    Made prefix const, made block a single string

--- examples/multicommand.cr
+++ examples/multicommand.cr
@@ -12,20 +12,22 @@ require "../src/discordcr"
 # Make sure to replace this fake data with actual data when running.
 client = Discord::Client.new(token: "Bot MjI5NDU5NjgxOTU1NjUyMzM3.Cpnz31.GQ7K9xwZtvC40y8MPY3eTqjEIXm", client_id: 229459681955652337_u64)
 
+# Command Prefix
+PREFIX = "!"
+
 client.on_message_create do |payload|
-  prefix = "!"
   command = payload.content
   case command
-  when prefix + "help"
+  when PREFIX + "help"
     client.create_message(client.create_dm(payload.author.id).id, "Help is on the way!")
-  when prefix + "about"
-    block = ["```\n", "Bot developed by discordcr\n", "```"]
-    client.create_message(payload.channel_id, block.join(""))
-  when .starts_with? prefix + "echo"
+  when PREFIX + "about"
+    block = "```\nBot developed by discordcr\n```"
+    client.create_message(payload.channel_id, block)
+  when .starts_with? PREFIX + "echo"
     # !echo is a good example of a command with arguments (suffix)
     suffix = command.split(' ')[1..-1].join(" ")
     client.create_message(payload.channel_id, suffix)
-  when prefix + "date"
+  when PREFIX + "date"
     client.create_message(payload.channel_id, Time.now.to_s("%D"))
   end
 end
