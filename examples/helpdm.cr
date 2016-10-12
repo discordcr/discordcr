cad4750fb00a2415bad2f64e64cb765044e3797c
Colin Rioux <colin350@gmail.com>
2016-10-12 12:51:33 -0400

    Formatted.

--- examples/helpdm.cr
+++ examples/helpdm.cr
@@ -1,13 +1,13 @@
-# This example will teach you explicitly how to send 
-# a "help" message to a user who requests it via direct message 
-# (dm) 
+# This example will teach you explicitly how to send
+# a "help" message to a user who requests it via direct message
+# (dm)
 
 require "../src/discordcr"
 
 # Make sure to replace this fake data with actual data when running.
 client = Discord::Client.new(token: "Bot MjI5NDU5NjgxOTU1NjUyMzM3.Cpnz31.GQ7K9xwZtvC40y8MPY3eTqjEIXm", client_id: 229459681955652337_u64)
 
-# this event is fired each time a new message is sent to the a text channel to any guild that the bot is connected to. 
+# this event is fired each time a new message is sent to the a text channel to any guild that the bot is connected to.
 client.on_message_create do |payload|
   prefix = "!"
   if payload.content.starts_with? prefix + "help"
@@ -16,4 +16,3 @@ client.on_message_create do |payload|
 end
 
 client.run
-
