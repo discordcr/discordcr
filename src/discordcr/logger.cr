c0660663e799af2f3ebc70a4efbf477a09c102c7
meew0 <blactbt@live.de>
2016-10-14 22:32:06 +0200

    Add a property for a logger's IO

--- src/discordcr/logger.cr
+++ src/discordcr/logger.cr
@@ -1,5 +1,10 @@
 require "logger"
 
+# The logger class is monkey patched to have a property for the IO.
+class Logger
+  property io
+end
+
 module Discord
   # The built in logger.
   LOGGER = Logger.new(STDOUT)
