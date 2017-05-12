d35a4e46f0fde3b125c846aa18f6981a8d21def3
z64 <zachnowicki@gmail.com>
2017-05-11 20:32:53 -0400

    add Oauth2Application and REST endpoint

---
+++ src/discordcr/mappings/oauth2.cr
@@ -0,0 +1,20 @@
+require "./converters"
+require "./user"
+
+module Discord
+  # An OAuth2 application, as registered with Discord, that can hold
+  # information about a `Client`'s associated bot user account and owner,
+  # among other OAuth2 properties.
+  struct OAuth2Application
+    JSON.mapping({
+      id:                     {type: UInt64, converter: SnowflakeConverter},
+      name:                   String,
+      icon:                   String?,
+      description:            String?,
+      rpc_origins:            Array(String)?,
+      bot_public:             Bool,
+      bot_require_code_grant: Bool,
+      owner:                  User,
+    })
+  end
+end
