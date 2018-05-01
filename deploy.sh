ea167e9b370dcd4f9f339dbe677b2bcfc138ba22
meew0 <blactbt@live.de>
2018-05-01 11:35:21 +0200

    Add a bunch of debug info to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,13 @@
 
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
+SOURCE_BRANCH="travis-tests"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
+  echo "Generating docs..."
   crystal doc
+  echo "Docs generated successfully."
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -20,12 +22,15 @@ fi
 
 if [ -n "$TRAVIS_TAG" ]; then
     SOURCE_BRANCH=$TRAVIS_TAG
+    echo "Tag build: $TRAVIS_TAG"
 fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
+
+echo "Cloning repo..."
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
@@ -34,6 +39,8 @@ cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
+echo "Creating directories..."
+
 mkdir -p out/doc/$SOURCE_BRANCH
 
 # Clean out existing contents
@@ -41,6 +48,8 @@ rm -rf out/doc/$SOURCE_BRANCH/**/* || exit 0
 
 # Run our compile script
 doCompile
+
+echo "Moving results..."
 
 # Move results
 mv docs/* out/doc/$SOURCE_BRANCH/
@@ -57,10 +66,14 @@ if [ -z "$DIFF_RESULT" ]; then
     exit 0
 fi
 
+echo "Committing..."
+
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+echo "Getting deploy key..."
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
@@ -72,5 +85,9 @@ chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
+echo "Pushing..."
+
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
+
+echo "All done!"
