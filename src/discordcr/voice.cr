8f1ab5cbd860f377424ccd3a69a6af672fbc97c5
r00ster <35064754+r00ster91@users.noreply.github.com>
2018-12-12 20:08:31 +0100

    Use monotonic clock

--- src/discordcr/voice.cr
+++ src/discordcr/voice.cr
@@ -284,9 +284,9 @@ module Discord
   # one frame every 20 ms, and if the processing and sending takes a certain
   # amount of time, then noticeable choppiness can be heard.
   def self.timed_run(total_time : Time::Span)
-    t1 = Time.now
+    time = Time.monotonic
     yield
-    delta = Time.now - t1
+    delta = Time.monotonic - time
 
     sleep_time = {total_time - delta, Time::Span.zero}.max
     sleep sleep_time
