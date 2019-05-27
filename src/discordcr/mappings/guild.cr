8ec4f9f2948b2b2b9b1e2d86626ed15f9814be24
r00ster <r00ster91@protonmail.com>
2019-05-27 17:02:31 +0200

    Add GamePlaying::Type
    Closes #188

--- src/discordcr/mappings/guild.cr
+++ src/discordcr/mappings/guild.cr
@@ -245,12 +245,20 @@ module Discord
   end
 
   struct GamePlaying
-    def initialize(@name = nil, @type = nil, @url = nil)
+    def initialize(@name = nil, @type : Type? = nil, @url = nil)
+    end
+
+    enum Type : UInt8
+      Playing   = 0
+      Streaming = 1
+      Listening = 2
+      Watching  = 3
+      Custom    = 4
     end
 
     JSON.mapping(
       name: String?,
-      type: Int64? | String?,
+      type: Type?,
       url: String?
     )
   end
